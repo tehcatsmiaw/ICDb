b70569e16c9a3710d46788c938e9f3e0c990d9b0
Andrei Locic <andrei.locic@gmail.com>
2023-08-15 12:32:39 +0100

    change api call to https

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import MovieCard from "./MovieCard";
 import "./App.css";
 import SearchIcon from "./search.svg";
 
-const API_URL = "http://www.omdbapi.com/?apikey=2bce77ec";
+const API_URL = "https://www.omdbapi.com/?apikey=2bce77ec";
 
 const App = () => {
   const [movies, setMovies] = useState([]);
